939f25b21d337bef15e7c7d4f968b4998382de48
Isa Anshori <isa.anshori.skom@gmail.com>
2026-08-12 16:34:53 +0700

    update_readme_dan_models

--- tapg_analytics/models/marts/z_scores.sql
+++ tapg_analytics/models/marts/z_scores.sql
@@ -1,3 +1,8 @@
+{{ config(
+    materialized='table',
+    cluster_by=['order_date']
+) }}
+
 with daily_revenue as (
 
     select * from {{ ref('daily_revenue') }}
@@ -5,8 +10,9 @@ with daily_revenue as (
 ),
 z_scores AS (
 SELECT
-    order_date,
+    purchase_date AS order_date,
     revenue,
+    AVG(revenue) OVER () as avg_revenue,
     (revenue - AVG(revenue) OVER ())/ STDDEV(revenue) OVER () AS z_score
 FROM daily_revenue
 )
